6b61c46e78ff0e8ffbb099cf70bd8bc872c4bfcd
fanggeyou <43359562+fanggeyou@users.noreply.github.com>
2019-02-14 15:52:45 -0500

    Update Index_Creation.sql

--- Index_Creation.sql
+++ Index_Creation.sql
@@ -1,38 +1,14 @@
--- Additional Index
-CREATE INDEX D_FNEX ON Directors(direct_fname);
-CREATE INDEX D_LNEX ON Directors(direct_lname);
-CREATE INDEX ADD_DATEX ON Queue(add_date);
-CREATE UNIQUE INDEX P_NAMEX ON Plan(plan_name);
-CREATE INDEX M_NAMEX ON Movie(movie_name);
-CREATE INDEX QUANT_EX ON DVD(quantity);
+REM
+REM DROP INDEXES
+REM
+DROP INDEX I_MemberName;
+DROP INDEX I_PersonName;
+DROP INDEX I_DVDTitle;
+REM
+REM CREATE INDEXES
+REM
+CREATE INDEX I_MemberName ON Member(MemberFirstName,MemberLastName);
+CREATE INDEX I_PersonName ON MoviePerson(PersonFirstName,PersonLastName);
+CREATE UNIQUE INDEX I_DVDTitle ON DVD(DVDTitle);
 
-
--- Index Foreign Keys
-CREATE INDEX P_IDEX ON Customers(plan_id);
-CREATE INDEX C_IDEX ON Queue(cust_id);
-CREATE INDEX M_IDEX ON Queue(movie_id);
-
-CREATE INDEX A_IDEX ON Act_Movie(actor_id);
-CREATE INDEX AM_IDEX ON Act_Movie(movie_id);
-CREATE INDEX DM_IDEX ON Direct_Movie(movie_id);
-CREATE INDEX D_IDEX ON Direct_Movie(direct_id);
-CREATE INDEX PRO_IDEX ON Produce_Movie(pro_id);
-CREATE INDEX PM_IDEX ON Produce_Movie(movie_id);
-
-CREATE INDEX DVDM_IDEX ON DVD(movie_id);
-CREATE INDEX DVDD_IDEX ON DVD(dist_id);
-CREATE INDEX RETURND_IDEX ON DVD_Return(dvd_id);
-CREATE INDEX RETURNR_IDEX ON DVD_Return(return_id);
-CREATE INDEX RT_C_IDEX ON Return_Transactions(cust_id);
-CREATE INDEX RT_E_IDEX ON Return_Transactions(emp_id);
-CREATE INDEX RT_D_IDEX ON Return_Transactions(deliver_id);
-CREATE INDEX RNT_C_IDEX ON Rental_Transactions(cust_id);
-CREATE INDEX RNT_E_IDEX ON Rental_Transactions(emp_id);
-CREATE INDEX RNT_D_IDEX ON Rental_Transactions(deliver_id);
-
-CREATE INDEX RENTD_IDEX ON DVD_Rent(dvd_id);
-CREATE INDEX RENTR_IDEX ON DVD_Rent(rent_id);
-CREATE INDEX DEPART_IDEX ON Employee(depart_id);
-
-
-commit;
+COMMIT;
