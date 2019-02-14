f332d8eff1f78cd86cdb838bdaaf361c8d5e3c34
fanggeyou <43359562+fanggeyou@users.noreply.github.com>
2019-02-14 15:48:07 -0500

    Add files via upload

---
+++ Fangge_You_CS779_Assign2.sql
@@ -0,0 +1,222 @@
+--Assignment 2.1
+--Q1--
+CREATE TABLE History(
+HistoryId	NUMBER(10)	NOT NULL,
+DVDID	NUMBER(16)	NOT NULL,
+MemberID NUMBER(12)	NOT NULL,
+CopyID	NUMBER(10) NOT NULL,
+RentalID NUMBER(16)	NOT NULL,
+MembershipId	NUMBER(10)	NOT NULL,
+RentalRequestDate DATE NOT NULL,
+RentalShippedDate DATE,
+RentalReturnedDate DATE,
+CONSTRAINT History_HistoryID_PK PRIMARY KEY (HistoryID),
+CONSTRAINT History_DVDID_FK FOREIGN KEY (DVDId) REFERENCES DVD(DVDId),
+CONSTRAINT History_MemberId_FK FOREIGN KEY (MemberId) REFERENCES Member(MemberId),
+CONSTRAINT History_CopyId_FK FOREIGN KEY (CopyId) REFERENCES DVD_Copy(CopyId),
+CONSTRAINT History_RentalId_FK FOREIGN KEY (RentalId) REFERENCES Rental(RentalId),
+CONSTRAINT History_MembershipId_FK FOREIGN KEY (MembershipId) REFERENCES Membership(MembershipId)
+);
+
+CREATE SEQUENCE history_ID --name
+START WITH 3
+INCREMENT BY 1
+NOCACHE;
+
+
+ALTER TABLE History ADD ChangeType VARCHAR(255);
+ALTER TABLE History ADD ChangeTime DATE;
+
+
+SELECT * FROM History;
+
+--Q2--
+CREATE OR REPLACE TRIGGER no_delete_history_trg
+BEFORE DELETE ON History
+BEGIN
+  raise_application_error (-20100, 'You can not delete records from this table');
+END;
+
+INSERT INTO history(historyid,dvdid,memberid,copyid,rentalid,membershipid,
+rentalrequestdate, rentalshippeddate, rentalreturneddate)
+VALUES(1,4,1,5,1,1,to_date('02/02/2004','MM/DD/YYYY'),
+	to_date('02/02/2004','MM/DD/YYYY'),to_date('02/09/2004','MM/DD/YYYY'));
+  
+DELETE FROM history WHERE dvdid = 4;
+UPDATE history SET changeType = 'INSERT', changetime = SYSDATE WHERE historyid = 1;
+
+--Q3--
+CREATE OR REPLACE TRIGGER update_shipdate_trg
+AFTER UPDATE OF rentalshippeddate, copyid ON Rental
+FOR EACH ROW
+DECLARE
+ms_id NUMBER(10);
+BEGIN
+    SELECT membershipid INTO ms_id FROM member WHERE memberid = :NEW.memberid;
+    INSERT INTO history(historyid,dvdid,memberid,copyid,rentalid,membershipid,
+    rentalrequestdate,rentalshippeddate,rentalreturneddate,changetype,changetime)
+    VALUES(history_id.NEXTVAL,:OLD.dvdid,:OLD.memberid,:NEW.copyid,:OLD.rentalid,
+    ms_id,:OLD.rentalrequestdate,:NEW.rentalshippeddate,:OLD.rentalreturneddate,
+    'UPDATE',SYSDATE);
+END;
+
+INSERT INTO dvd_copy(copyid,dvdid,copystatus) VALUES(9,7,'Rented');
+
+UPDATE rental SET rentalshippeddate = to_date('03/05/2004','MM/DD/YYYY'), copyid = 9
+WHERE rentalid = 10;
+
+SELECT * FROM history;
+
+
+--Q4--
+CREATE OR REPLACE TRIGGER update_returndate_trg
+AFTER UPDATE OF rentalreturneddate ON Rental
+FOR EACH ROW
+DECLARE
+ms_id NUMBER(10);
+BEGIN
+    SELECT membershipid INTO ms_id FROM member WHERE memberid = :NEW.memberid;
+    INSERT INTO history(historyid,dvdid,memberid,copyid,rentalid,membershipid,
+    rentalrequestdate,rentalshippeddate,rentalreturneddate,changetype,changetime)
+    VALUES(history_id.NEXTVAL,:OLD.dvdid,:OLD.memberid,:NEW.copyid,:OLD.rentalid,
+    ms_id,:OLD.rentalrequestdate,:OLD.rentalshippeddate,:NEW.rentalreturneddate,
+    'UPDATE',SYSDATE);
+END;
+
+
+UPDATE rental SET rentalreturneddate = to_date('03/05/2004','MM/DD/YYYY')
+WHERE rentalid = 8;
+
+SELECT * FROM history;
+
+
+
+--Q5--
+CREATE OR REPLACE TRIGGER no_more_movie_trg
+BEFORE UPDATE OF rentalshippeddate ON Rental
+FOR EACH ROW
+DECLARE num_onhand NUMBER(10); dvd_num NUMBER(10); PRAGMA AUTONOMOUS_TRANSACTION;
+BEGIN
+  SELECT COUNT(memberid) INTO num_onhand
+  FROM rental WHERE rentalreturneddate IS NULL AND memberid = :OLD.memberid
+  GROUP BY memberid;
+  SELECT DVDATTIME INTO dvd_num FROM membership JOIN member 
+  ON membership.membershipid = member.membershipid
+  WHERE member.memberid = :OLD.memberid;
+  IF num_onhand > dvd_num THEN 
+    raise_application_error (-20010, 'You have reached monthly DVD limits');
+  
+  END IF;
+END;
+
+DELETE FROM rental WHERE rentalid = 13;
+
+INSERT INTO dvd_copy(copyid,dvdid,copystatus) VALUES(10,5,'Rented');
+INSERT INTO dvd_copy(copyid,dvdid,copystatus) VALUES(11,4,'Rented');
+
+INSERT INTO rental(rentalid,memberid,dvdid,rentalrequestdate,rentalshippeddate,
+rentalreturneddate,copyid) 
+VALUES(11,15,1,to_date('03/08/2004','MM/DD/YYYY'),to_date('03/09/2004','MM/DD/YYYY'),NULL,3);
+INSERT INTO rental(rentalid,memberid,dvdid,rentalrequestdate,rentalshippeddate,
+rentalreturneddate,copyid) 
+VALUES(12,15,5,to_date('03/08/2004','MM/DD/YYYY'),to_date('03/09/2004','MM/DD/YYYY'),NULL,10);
+INSERT INTO rental(rentalid,memberid,dvdid,rentalrequestdate,rentalshippeddate,
+rentalreturneddate,copyid) 
+VALUES(13,15,4,to_date('03/08/2004','MM/DD/YYYY'),NULL,NULL,11);
+
+COMMIT;
+
+SELECT * FROM rental;
+
+UPDATE rental SET rentalshippeddate = to_date('03/10/2004','MM/DD/YYYY')
+WHERE rentalid = 13;
+
+
+--Q6--
+ALTER TABLE rentalqueue ADD queuerank NUMBER(10);
+
+CREATE OR REPLACE PROCEDURE ADD_QUEUE (
+m_id_arg IN NUMBER,
+d_id_arg IN NUMBER,
+rank_arg IN NUMBER,
+m_d OUT VARCHAR)
+IS 
+BEGIN
+  SELECT CONCAT(memberid,dvdid) INTO m_d 
+  FROM rentalqueue WHERE memberid = m_id_arg AND dvdid = d_id_arg;
+    raise_application_error (-20010, 'You have already added this movie');
+  EXCEPTION WHEN NO_DATA_FOUND THEN
+    INSERT INTO rentalqueue(memberid, dvdid, dateaddedinqueue, queuerank)
+    VALUES(m_id_arg, d_id_arg, SYSDATE, rank_arg);
+END;
+/
+
+
+
+SET SERVEROUTPUT ON
+DECLARE
+   m_d VARCHAR(20);
+BEGIN
+  ADD_QUEUE(m_id_arg=>12,d_id_arg=>5,rank_arg=>1,m_d=>m_d);
+  
+END;
+/
+
+
+SET SERVEROUTPUT ON
+DECLARE
+  m_d VARCHAR(20);
+BEGIN
+  ADD_QUEUE(m_id_arg=>3,d_id_arg=>1,rank_arg=>1,m_d=>m_d);
+  
+END;
+/
+
+SELECT * FROM rentalqueue;
+
+
+
+
+--Q7--
+CREATE OR REPLACE PROCEDURE REMOVE_QUEUE (
+m_id_arg IN NUMBER,
+d_id_arg IN NUMBER,
+rank_num OUT NUMBER)
+IS 
+BEGIN
+  SELECT queuerank INTO rank_num
+  FROM rentalqueue WHERE memberid = m_id_arg AND dvdid = d_id_arg;
+  DELETE FROM rentalqueue WHERE memberid = m_id_arg AND dvdid = d_id_arg;
+  FOR line IN (SELECT memberid, queuerank FROM rentalqueue 
+  WHERE memberid = m_id_arg AND queuerank > rank_num) LOOP
+  UPDATE rentalqueue SET queuerank := queuerank-1;
+  END LOOP;
+END;
+/
+
+
+INSERT INTO rentalqueue(memberid, dvdid, dateaddedinqueue, queuerank)
+VALUES (1,3,to_date('04/08/2004','MM/DD/YYYY'),1);
+INSERT INTO rentalqueue(memberid, dvdid, dateaddedinqueue, queuerank)
+VALUES (1,4,to_date('05/16/2004','MM/DD/YYYY'),2);
+INSERT INTO rentalqueue(memberid, dvdid, dateaddedinqueue, queuerank)
+VALUES (1,6,to_date('05/22/2004','MM/DD/YYYY'),3);
+
+SELECT * FROM rentalqueue;
+
+
+SET SERVEROUTPUT ON
+DECLARE
+  rank_num NUMBER(10);
+BEGIN
+  REMOVE_QUEUE(m_id_arg=>1,d_id_arg=>4,rank_num=>rank_num);
+  
+END;
+/
+
+
+
+COMMIT;
+
+
+
---
+++ Fangge_You_CS77_Assign2.sql
@@ -0,0 +1,258 @@
+-- Assignment 2.0
+-- Part B
+-- Q1 --
+CREATE MATERIALIZED VIEW Similar_DVD
+REFRESH FORCE  -- review refresh increment
+START WITH SYSDATE NEXT SYSDATE+1
+WITH PRIMARY KEY
+AS
+SELECT DVD.DVDTitle AS "DVD Title",  
+		SUBSTR(MoviePerson.PersonFirstName,1,10) || ' ' ||
+		SUBSTR(MoviePerson.PersonLastName,1,10) AS "Director Name"
+FROM DVD, MoviePersonRole, Role, MoviePerson
+WHERE DVD.DVDId = MoviePersonRole.DVDId
+		AND MoviePersonRole.RoleId = Role.RoleId
+		AND MoviePersonRole.PersonId = MoviePerson.PersonId
+		AND Role.RoleName = 'Director'
+		AND ((MoviePerson.PersonLastName = 'Spielberg') 
+		     OR (MoviePerson.PersonLastName = 'Hitchcock' 
+		AND MoviePerson.PersonFirstName = 'Alfred'));
+;
+
+
+SELECT DVD.DVDTitle AS "DVD Title",  
+		SUBSTR(MoviePerson.PersonFirstName,1,10) || ' ' ||
+		SUBSTR(MoviePerson.PersonLastName,1,10) AS "Director Name"
+FROM DVD, MoviePersonRole, Role, MoviePerson
+WHERE DVD.DVDId = MoviePersonRole.DVDId
+		AND MoviePersonRole.RoleId = Role.RoleId
+		AND MoviePersonRole.PersonId = MoviePerson.PersonId
+		AND Role.RoleName = 'Director'
+		AND ((MoviePerson.PersonLastName = 'Spielberg') 
+		     OR (MoviePerson.PersonLastName = 'Hitchcock' 
+		AND MoviePerson.PersonFirstName = 'Alfred'));
+    
+--solution    
+CREATE INDEX Person_FL ON MoviePerson (PersonFirstName, PersonLastName);  
+
+CREATE INDEX Role_name ON Role (RoleName);
+
+   
+SELECT DVD.DVDTitle AS "DVD Title",  
+		SUBSTR(MoviePerson.PersonFirstName,1,10) || ' ' ||
+		SUBSTR(MoviePerson.PersonLastName,1,10) AS "Director Name"
+FROM DVD 
+JOIN MoviePersonRole ON DVD.DVDId = MoviePersonRole.DVDId
+JOIN Role ON MoviePersonRole.RoleId = Role.RoleId
+JOIN MoviePerson ON MoviePersonRole.PersonId = MoviePerson.PersonId
+WHERE Role.RoleName = 'Director' AND ((MoviePerson.PersonLastName = 'Spielberg') 
+OR (MoviePerson.PersonLastName = 'Hitchcock' 
+AND MoviePerson.PersonFirstName = 'Alfred'));
+
+
+--Q2--
+SELECT DVDTitle AS "DVD Title"
+FROM DVD 
+WHERE DVDId NOT IN 
+(SELECT DISTINCT DVDId FROM Rental WHERE MemberId = 123);
+
+--solution
+CREATE INDEX DVD_index ON Rental (DVDID);
+
+CREATE INDEX Member_index ON Rental (MemberID);
+
+
+WITH r AS
+(SELECT dvdid FROM rental WHERE memberid = 123
+GROUP BY dvdid)
+SELECT d.DVDTitle AS "DVD Title"
+FROM DVD d LEFT OUTER JOIN r ON d.dvdid = r.dvdid
+WHERE r.dvdid IS NULL;
+
+
+
+--Q3--
+SELECT DVDId, DVDTitle AS "DVD Title"
+FROM DVD
+WHERE DVDId IN (SELECT DVDId FROM Rental 
+			WHERE RentalReturnedDate IS NULL);
+
+--solution
+CREATE INDEX Return_Date_index ON Rental (rentalReturnedDate,1);
+
+SELECT dvd.DVDId, dvd.DVDTitle AS "DVD Title"
+FROM DVD JOIN rental ON dvd.dvdid = rental.dvdid
+WHERE rental.RentalReturnedDate IS NULL;
+
+
+--Q4--
+SELECT SUBSTR(Member.MemberFirstName,1,10) || ' ' || 
+		SUBSTR(Member.MemberLastName,1,10) AS Name,
+		SUBSTR(Member.MemberAddress,1,30) AS Address, 
+		SUBSTR(City.CityName,1,12) AS City,
+		SUBSTR(State.StateName,1,12) AS State,
+ 		Zipcode.ZipCode AS Zip 
+FROM Member, City, State, ZipCode 
+WHERE Member.MemberFirstName = 'Susan'
+AND Member.MemberLastName = 'Smith' 
+AND Member.MemberAddressId = ZipCode.ZipCodeId
+AND ZipCode.CityId = City.CityId
+AND ZipCode.StateId = State.StateId;
+
+
+--solution
+CREATE INDEX Member_name_index ON member (MemberFirstName, MemberLastName);
+
+
+SELECT SUBSTR(Member.MemberFirstName,1,10) || ' ' || 
+		SUBSTR(Member.MemberLastName,1,10) AS Name,
+		SUBSTR(Member.MemberAddress,1,30) AS Address, 
+		SUBSTR(City.CityName,1,12) AS City,
+		SUBSTR(State.StateName,1,12) AS State,
+ 		Zipcode.ZipCode AS Zip 
+FROM Member
+JOIN Zipcode ON Member.MemberAddressId = ZipCode.ZipCodeId
+JOIN City ON ZipCode.CityId = City.CityId
+JOIN State ON ZipCode.StateId = State.StateId
+WHERE Member.MemberFirstName = 'Susan'
+AND Member.MemberLastName = 'Smith' ;
+
+
+--Q5--
+SELECT DISTINCT DVD.DVDId, DVD.DVDTitle, Genre.GenreName 
+FROM Rental, DVD, Genre
+WHERE MemberId = 
+(SELECT MemberId FROM Member
+WHERE MemberFirstName = 'Alfred'
+AND MemberLastName = 'Newman')
+	AND Rental.DVDId = DVD.DVDId
+AND DVD.GenreId = Genre.GenreId
+	AND Genre.GenreName = 'Horror';
+
+--solution
+CREATE INDEX genre_name_index ON Genre (GenreName);
+
+SELECT DISTINCT DVD.DVDId, DVD.DVDTitle, Genre.GenreName 
+FROM Rental
+JOIN DVD ON Rental.DVDId = DVD.DVDId
+JOIN Genre ON DVD.GenreId = Genre.GenreId
+JOIN Member ON rental.memberid = member.memberid
+WHERE member.MemberFirstName = 'Alfred'
+AND member.MemberLastName = 'Newman'
+AND Genre.GenreName = 'Horror';
+
+
+
+--Q6--
+CREATE VIEW DVDView AS 
+		SELECT DVDId, DVDTitle, Genre.GenreName AS Genre, 
+			Rating.RatingName AS Rating
+		FROM DVD, Genre, Rating 
+		WHERE DVD.GenreId = Genre.GenreId
+		AND DVD.RatingId = Rating.RatingId;
+
+--solution
+CREATE INDEX dvd_genre_index ON DVD (GenreId);
+CREATE INDEX dvd_rating_index ON DVD (RatingId);
+
+
+CREATE VIEW DVDView AS 
+		SELECT DVDId, DVDTitle, Genre.GenreName AS Genre, 
+			Rating.RatingName AS Rating
+		FROM DVD
+    JOIN Genre ON DVD.GenreId = Genre.GenreId
+    JOIN Rating ON DVD.RatingId = Rating.RatingId;
+
+
+
+--Q7--
+CREATE OR REPLACE PROCEDURE Lost_DVD -- P prefix denotes parameters
+(P_RentalId IN NUMBER, -- The rental for which DVD was lost
+	P_MemberId IN NUMBER, -- The member
+	P_Amount IN NUMBER, -- The amount to charge
+	P_Trans_Date IN DATE) --The transaction date and time
+AS -- The V prefix denotes variables.
+V_DVDId NUMBER := 0.0;
+V_PaymentId NUMBER := 0;
+V_UntilDate DATE := NULL;
+
+BEGIN
+
+-- Get DVDId
+      SELECT DVDId into V_DVDId From RENTAL WHERE RentalId = P_RentalId;
+
+-- Get PaymentId
+       SELECT COUNT(*)+1 into V_PaymentId FROM Payment;
+	
+-- Get AmountPaidUntilDate
+	SELECT AmountPaidUntilDate INTO V_UntilDate FROM Payment 
+		WHERE MemberId = P_MemberId
+AND PaymentId = 
+(SELECT MAX(PaymentId) FROM Payment WHERE MemberId=P_MemberId);
+
+-- Now, make the changes to the database…
+INSERT INTO Payment(PaymentId, MemberId, AmountPaid,
+     	AmountPaidDate,AmountPaidUntilDate)
+	VALUES(V_PaymentId,P_MemberId,P_Amount,P_Trans_Date,V_UntilDate);
+
+
+UPDATE Rental SET RentalReturnedDate = P_Trans_Date
+WHERE RentalId = P_RentalId;
+
+UPDATE DVD SET DVDQuantityOnRent = DVDQuantityOnRent - 1
+WHERE DVDId = V_DVDId;
+
+UPDATE DVD SET DVDLostQuantity = DVDLostQuantity + 1
+WHERE DVDId = V_DVDId;
+
+END Lost_DVD;
+--solution
+CREATE SEQUENCE payment_ID --name
+START WITH 17
+INCREMENT BY 1
+NOCACHE;
+
+CREATE OR REPLACE PROCEDURE Lost_DVD -- P prefix denotes parameters
+(P_RentalId IN NUMBER, -- The rental for which DVD was lost
+	P_MemberId IN NUMBER, -- The member
+	P_Amount IN NUMBER, -- The amount to charge
+	P_Trans_Date IN DATE) --The transaction date and time
+AS -- The V prefix denotes variables.
+V_DVDId NUMBER := 0.0;
+V_UntilDate DATE := NULL;
+BEGIN
+-- Get DVDId
+      SELECT DVDId into V_DVDId From RENTAL WHERE RentalId = P_RentalId;
+-- Get AmountPaidUntilDate
+	SELECT p1.AmountPaidUntilDate INTO V_UntilDate FROM Payment p1
+		JOIN Member ON p1.memberid = member.memberid
+    LEFT OUTER JOIN Payment p2 ON p1.memberid = p2.memberid
+    WHERE member.MemberId = 1
+    AND p1.paymentid > p2.paymentid;
+-- Now, make the changes to the database…
+INSERT INTO Payment(PaymentId, MemberId, AmountPaid,
+     	AmountPaidDate,AmountPaidUntilDate)
+	VALUES(payment_ID.NEXTVAL,P_MemberId,P_Amount,P_Trans_Date,V_UntilDate);
+
+UPDATE Rental SET RentalReturnedDate = P_Trans_Date
+WHERE RentalId = P_RentalId;
+
+UPDATE DVD SET DVDQuantityOnRent = DVDQuantityOnRent - 1
+WHERE DVDId = V_DVDId;
+
+UPDATE DVD SET DVDLostQuantity = DVDLostQuantity + 1
+WHERE DVDId = V_DVDId;
+
+END Lost_DVD;
+
+
+
+SELECT p1.AmountPaidUntilDate FROM Payment p1
+		JOIN Member ON p1.memberid = member.memberid
+    LEFT OUTER JOIN Payment p2 ON p1.memberid = p2.memberid
+    WHERE member.MemberId = 1
+    AND p1.paymentid > p2.paymentid;
+
+
+
+COMMIT;
